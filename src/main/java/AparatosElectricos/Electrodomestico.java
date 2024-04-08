290ecb04b3aee64cc70122b32c598ca9de20d2b7
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2024-04-08 19:52:55 +0200

    Classe electrodomestic avanzada

--- src/main/java/AparatosElectricos/Electrodomestico.java
+++ src/main/java/AparatosElectricos/Electrodomestico.java
@@ -8,30 +8,78 @@ package AparatosElectricos;
  *
  * @author mark
  */
-public class Electrodomestico extends  AparatoElectrico {
+public abstract class Electrodomestico extends AparatoElectrico {
+
+    protected float precioBase;
+    private final float PRECIO_DEFAULT = 100;
+
+    protected String marca;
+    protected String modelo;
+    protected Color color;
+    protected ConsumoEnergetico consumoEnerg;
+    private final float PRECIO_AÑADIDO_A = 100;
+    private final float PRECIO_AÑADIDO_B = 80;
+    private final float PRECIO_AÑADIDO_C = 60;
+    private final float PRECIO_AÑADIDO_D = 50;
+    private final float PRECIO_AÑADIDO_E = 30;
+    private final float PRECIO_AÑADIDO_F = 10;
     
-     protected float precioBase;
-     protected String marca;
-     protected String modelo;
-     protected Color color;
-     protected ConsumoEnergetico consumoEnerg;
-     protected int peso;
     
-    public Electrodomestico(String numSerie){
-       super(numSerie);
-       //
-       
-       
+    protected int peso;
+    private final int PESO_DEFAULT = 5;
+
+    public Electrodomestico(String numSerie, float precioBase, String marca, String modelo, Color color, ConsumoEnergetico consumoEnerg, int peso) {
+        super(numSerie);
+        this.precioBase = precioBase;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.consumoEnerg = consumoEnerg;
+        this.peso = peso;
+    }
+
+    public Electrodomestico(String numSerie, String marca, String modelo) {
+        super(numSerie);
+
+        this.marca = marca;
+        this.modelo = modelo;
+
+        this.color = Color.BLANCO;
+        this.consumoEnerg = ConsumoEnergetico.F;
+        this.precioBase = PRECIO_DEFAULT;
+        this.peso = PESO_DEFAULT;
+    }
+
+    protected enum Color {
+        BLANCO, NEGRO, ROJO, AZUL, GRIS
+    }
+
+    protected enum ConsumoEnergetico {
+        A, B, C, D, E, F
     }
     
-    private enum Color{
-        BLANCO,NEGRO,ROJO,AZUL,GRIS
+    public float obtenerPrecioVenta(){
+        return this.precioBase + getIncrementoConsumoEnerg(this.consumoEnerg);
     }
     
-    private enum ConsumoEnergetico{
-        A,B,C,D,E,F
+    private float getIncrementoConsumoEnerg(ConsumoEnergetico consumoE){
+        switch(consumoE){
+            case A:
+                return PRECIO_AÑADIDO_A;
+            case B:
+                return PRECIO_AÑADIDO_B;    
+            case C:
+                return PRECIO_AÑADIDO_C;
+            case D:
+                return PRECIO_AÑADIDO_D;
+            case E:
+                return PRECIO_AÑADIDO_E;    
+            case F:
+                return PRECIO_AÑADIDO_F;
+            default:
+                return 0;
+        }
     }
     
+
 }
-
-
