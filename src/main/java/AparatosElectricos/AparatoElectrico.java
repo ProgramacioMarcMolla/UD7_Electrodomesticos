cce047051bcc93158e5ff9b25160a9d3c467ae5d
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2024-04-08 19:16:21 +0200

    Empezando proyecto

---
+++ src/main/java/AparatosElectricos/AparatoElectrico.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AparatosElectricos;
+
+/**
+ *
+ * @author mark
+ */
+public abstract class AparatoElectrico {
+    
+    protected String numSerie;
+    
+    public AparatoElectrico(String numSerie){
+        this.numSerie = numSerie;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/AparatosElectricos/Electrodomestico.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AparatosElectricos;
+
+/**
+ *
+ * @author mark
+ */
+public class Electrodomestico extends  AparatoElectrico {
+    
+     protected float precioBase;
+     protected String marca;
+     protected String modelo;
+     protected Color color;
+     protected ConsumoEnergetico consumoEnerg;
+     protected int peso;
+    
+    public Electrodomestico(String numSerie){
+       super(numSerie);
+       
+       
+       
+    }
+    
+    private enum Color{
+        BLANCO,NEGRO,ROJO,AZUL,GRIS
+    }
+    
+    private enum ConsumoEnergetico{
+        A,B,C,D,E,F
+    }
+    
+}
+
+
